7b81266baacffd0c25533ffa3dd31937abc89655
saksham <phantomdx316@gmail.com>
2023-12-22 00:18:57 +0530

    added the header content

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import styles from "./Footer.module.css"
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className={`${styles.footer}`}>
+      <div className={`${styles.footerContent} row`}>
+        <div className={`${styles.footerSocialLinks} col-md-4`}>
+            <div className={`${styles.footerSocialLinksContent} row justify-content-center align-items-center`}>
+              <div className={`col-md-12 text-center`}>
+                <strong style={{color:"white",fontFamily:""}}>Appointment just one click away</strong>
+              </div>
+            </div>
+        </div>
+        <div className={`${styles.footerTabs} col-md-8`}></div>
+      </div>
     </div>
   )
 }
