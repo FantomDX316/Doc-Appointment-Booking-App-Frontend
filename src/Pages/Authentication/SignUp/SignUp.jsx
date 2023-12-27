5776e6eebdbe543447ab2631ff172bd19a5045b2
saksham <phantomdx316@gmail.com>
2023-12-27 22:46:53 +0530

    added field in the sign up card

--- src/Pages/Authentication/SignUp/SignUp.jsx
+++ src/Pages/Authentication/SignUp/SignUp.jsx
@@ -2,12 +2,17 @@
 import React from 'react'
 import styles from "./SignUp.module.css"
 import { useForm } from "react-hook-form";
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import Select from "react-select";
 
 // -----------------------------------------------------------------------------------------------------------------------------------
 
 const SignUp = () => {
   // ----------------------------------------------------------------States---------------------------------------------------------------
+
+  //roleOptions -- options for the roles dropdown
+  const roleOptions = [{ value: "user", label: "User" },
+  { value: "counselor", label: "Counselor" }]
 
   // -------------------------------------------------------------------------------------------------------------------------------------
   // ----------------------------------------------------------------Hooks---------------------------------------------------------------
@@ -39,31 +44,45 @@ const SignUp = () => {
   return (
     <div className={`${styles.signupContainer}`}>
       <div className={`${styles.signupWrapper}`}>
-        <div className={`${styles.signupCard} col-md-4`}>
+        <div className={`${styles.signupCard} col-md-5`}>
           <div className={`${styles.signupContent}`}>
             <div className="topContent col-md-12 col-12 col-sm-12 d-flex flex-column align-items-center">
               <div className={` col-md-12 col-12 col-sm-12 text-center p-2`}>
                 <h1 className={`${styles.signupCardTitle}`}>Sign Up</h1>
               </div>
               <div className="noAccount">
-                <h6>Already Have an account? <span><u onClick={()=>{navigate("/login")}} style={{cursor:"pointer"}}>Log In</u></span></h6>
+                <h6>Already Have an account? <span><u onClick={() => { navigate("/login") }} style={{ cursor: "pointer" }}>Log In</u></span></h6>
               </div>
 
             </div>
             <div className="midContent col-md-12 col-12 col-sm-12 p-4">
               <form onSubmit={handleSubmit(signupHandler)} className={`${styles.signupForm} col-md-12 d-flex flex-column mt-4`}>
-                <div className={`col-md-12 col-sm-12 col-12 text-center m-2`}>
-                  <input type="text" id="email" placeholder='Email' className={`${styles.emailInput}`} />
-                </div>
-                <div className={`col-md-12 col-sm-12 col-12 text-center m-2`}>
-                  <input type="password" id="password" placeholder='Password' className={`${styles.passwordInput}`} />
-                </div>
-                <div className={`col-md-12 col-sm-12 col-12 text-center m-2`}>
-                  <input type="password" id="password" placeholder='Confirm Password' className={`${styles.confirmPasswordInput}`} />
-                </div>
-                <div className="signupButtonContent col-md-12 col-12 col-sm-12 text-center mt-5">
-                  <div className="signupButton">
-                    <button type="submit" className={`${styles.signupButtonEle}`}>SIGN UP</button>
+                <div className="row">
+                  <div className={`col-md-6 col-sm-6 col-12 text-center`}>
+                    <input type="text" id="firstName" placeholder='First Name' className={`${styles.nameField}`} />
+                  </div>
+                  <div className={`col-md-6 col-sm-6 col-12 text-center `}>
+                    <input type="text" id="lastName" placeholder='Last Name' className={`${styles.nameField}`} />
+                  </div>
+                  <div className={`col-md-6 col-sm-6 col-12 text-center mt-3`}>
+                    <input type="email" id="email" placeholder='Email' className={`${styles.nameField}`} />
+                  </div>
+                  <div className={`col-md-6 col-sm-6 col-12 text-center mt-3`}>
+                    <input type="number" id="phone" placeholder='Phone' className={`${styles.nameField}`} />
+                  </div>
+                  <div className={`col-md-6 col-sm-6 col-12 text-center mt-3`}>
+                    <input type="password" id="password" placeholder='Password' className={`${styles.nameField}`} />
+                  </div>
+                  <div className={`col-md-6 col-sm-6 col-12 text-center mt-3`}>
+                    <input type="password" id="confirmPassword" placeholder='Confirm Password' className={`${styles.nameField}`} />
+                  </div>
+                  <div className={`col-md-12 col-sm-12 col-12 text-center mt-3 d-flex justify-content-center`}>
+                    <Select options={roleOptions} value={"he;lo"} className={`${styles.inputField}`}/>
+                  </div>
+                  <div className="signupButtonContent col-md-12 col-12 col-sm-12 text-center mt-5">
+                    <div className="signupButton">
+                      <button type="submit" className={`${styles.signupButtonEle}`}>SIGN UP</button>
+                    </div>
                   </div>
                 </div>
               </form>
