19e6062b1b35f7340346f23425d817f451ff8ffe
saksham <phantomdx316@gmail.com>
2023-12-29 21:40:46 +0530

    added custom styling to the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           <div className={`${styles.homeContentWrapper}`}>
             <h1>Book Appointment and Trust us with that</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis similique nulla necessitatibus dignissimos doloribus quis quam dolor fuga corrupti vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos perspiciatis at reprehenderit id exercitationem earum numquam, ipsam ad dolorum itaque rem repellat neque blanditiis pariatur possimus iure consequuntur voluptatem ullam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, voluptatum. Ipsum voluptatum ut cupiditate accusantium, et aliquam,</p>
-            <div className="seeMoreBtn">
+            <div className={`${styles.seeMoreBtn} col-md-12 col-sm-12 col-12 text-center`}>
               <input type="button" name="See More" value="See More"/>
             </div>
           </div>
