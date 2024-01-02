3a9fe58b6aa3b39741e337c773e6ec567c4bceea
saksham <phantomdx316@gmail.com>
2024-01-02 07:28:35 +0530

    added the category card

--- src/Pages/Therapy/Therapy.jsx
+++ src/Pages/Therapy/Therapy.jsx
@@ -20,12 +20,28 @@ const Therapy = () => {
                     <IoMdArrowRoundBack className={`${styles.backIcon}`} />
                 </div>
                 <div className={`${styles.topTitle} text-center`}>
-                    <h1>Who do you want to consult</h1>
+                    <h1>Who do you want to consult?</h1>
                     <h6>Therapy can help you cope with emotional struggles that are making it difficult for you to function well.</h6>
                 </div>
             </div>
             <div className={`${styles.therapyMidContent}`}>
-                <div className={`${styles.theraptCategories}`}></div>
+                <div className={`${styles.therapyCategoriesContainer}`}>
+                    {Array(6).fill(0).map(() => {
+                        return (
+                            <div className={`${styles.therapyCategoryCard} col-md-5 col-sm-5 col-12`}>
+                                <div className={`${styles.therapyCategoryCardContentWrapper}`}>
+                                    <div className={`${styles.therapyCategoryCardImg} `}>
+
+                                    </div>
+                                    <div className={`${styles.therapyCategoryCardContent}`}>
+
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+                </div>
             </div>
         </div>
     )
