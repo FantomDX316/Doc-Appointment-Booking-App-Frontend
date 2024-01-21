387edd6443ea3e949387c4e663d54ebc01a593a4
saksham <phantomdx316@gmail.com>
2024-01-21 19:10:59 +0530

    added the form validation

--- src/Components/Form/CreateBlogForm/CreateBlogForm.js
+++ src/Components/Form/CreateBlogForm/CreateBlogForm.js
@@ -1,9 +1,43 @@
 // --------------------------------------------------------------Imports------------------------------------------------------------
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import styles from "./CreateBlogForm.module.css";
 // ------------------------------------------------------------------------------------------------------------------------------------
 
 const CreateBlogForm = () => {
+
+    // ----------------------------------------------------------------States---------------------------------------------------------------
+
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------Hooks---------------------------------------------------------------
+    const {
+        register,
+        reset,
+        formState: { errors },
+        handleSubmit
+    } = useForm()
+
+    const navigate = useNavigate();
+
+
+    // dispatch -- hook to dispatch the actions
+    const dispatch = useDispatch();
+
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------Functions--------------------------------------------------------------
+
+    // createBlogHandler -- handler to call the create blog api
+    const createBlogHandler = () => {
+
+    }
+
+
+    // -------------------------------------------------------------------------------------------------------------------------------------
+    // --------------------------------------------------------------useEffects-----------------------------------------------------------
+
+    // -------------------------------------------------------------------------------------------------------------------------------------
     return (
 
         <>
@@ -11,7 +45,7 @@ const CreateBlogForm = () => {
                 <div className="container">
                     <div className="row">
                         <div className="card card-signup" data-background-color="orange">
-                            <form className="form" method="" action="">
+                            <form className="form" onSubmit={handleSubmit(createBlogHandler)}>
                                 <div className="card-header text-center">
                                     <h3 className="card-title title-up">Create Blog</h3>
                                 </div>
@@ -25,9 +59,17 @@ const CreateBlogForm = () => {
                                             </span>
                                         </div>
                                         <div className="input-group-prepend col-md-8 col-sm-12 col-12 m-2">
-                                            <input type="text" className="form-control" placeholder="Title..." />
+                                            <input type="text" className="form-control" placeholder="Title..." {...register("blogTitle", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Blog Title is a required field"
+                                                }
+                                            })} />
                                         </div>
                                     </div>
+                                    {errors.blogTitle && errors.blogTitle.type === "required" && (
+                                        <p className={`${styles.nameFieldError} col-md-11`}>Blog Title is required.</p>
+                                    )}
                                     <div className={`${styles.descriptionContent} col-md-12 col-12 col-sm-12 d-flex m-3`}>
                                         <div className="input-group-prepend col-md-4 m-2">
                                             <span className="input-group-text">
@@ -35,9 +77,18 @@ const CreateBlogForm = () => {
                                             </span>
                                         </div>
                                         <div className="input-group-prepend  col-md-8 m-2">
-                                            <textarea type="text" className="form-control" placeholder="Description..." />
+                                            <textarea type="text" className="form-control" placeholder="Description..." {...register("blogDescription", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Blog Description is a required field"
+                                                }
+                                            })} />
                                         </div>
                                     </div>
+                                    {errors.blogDescription && errors.blogDescription.type === "required" && (
+                                        <p className={`${styles.nameFieldError} col-md-11`}>Blog Description is required.</p>
+                                    )}
+
                                     <div className={`${styles.blogImage} col-md-12 col-12 col-sm-12 d-flex m-3`}>
                                         <div className="input-group-prepend col-md-4 m-2">
                                             <span className="input-group-text">
@@ -45,9 +96,17 @@ const CreateBlogForm = () => {
                                             </span>
                                         </div>
                                         <div className="input-group-prepend  col-md-8 m-2">
-                                            <input type="file" className="form-control" accept='image/jpg' />
+                                            <input type="file" className="form-control" accept='image/jpg' {...register("blogImage", {
+                                                required: {
+                                                    value: true,
+                                                    message: "Blog Image is a required field"
+                                                }
+                                            })} />
                                         </div>
                                     </div>
+                                    {errors.blogImage && errors.blogImage.type === "required" && (
+                                        <p className={`${styles.nameFieldError} col-md-11`}>Blog Image is required.</p>
+                                    )}
                                     <div className={`${styles.blogImage} col-md-12 col-12 col-sm-12 d-flex justify-content-center m-3`}>
                                         <div className="input-group-prepend col-md-4 m-2 d-flex justify-content-center">
                                             <input type="submit" className="input-group-text" value='Create Blog' />
