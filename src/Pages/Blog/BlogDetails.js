8c85181713c13c6678620bf4e9bb0277f2a64def
saksham <phantomdx316@gmail.com>
2024-01-30 00:23:34 +0530

    created the blog details page

--- src/Pages/Blog/BlogDetails.js
+++ src/Pages/Blog/BlogDetails.js
@@ -1,6 +1,7 @@
 // ------------------------------------------------------------Imports------------------------------------------------------
 import React from 'react';
 import styles from "./BlogDetails.module.css";
+import bgImg from "../../Assets/Images/bgImg.jpg"
 // --------------------------------------------------------------------------------------------------------------------------
 
 const BlogDetails = () => {
@@ -13,8 +14,8 @@ const BlogDetails = () => {
                 <div className="blogCreatorDetails col-md-12 col-12 col-sm-12">
                     blogCreatorDetails
                 </div>
-                <div className="blogImage col-md-12 col-12 col-sm-12">
-                    blogImage
+                <div className={`${styles.blogImage} col-md-12 col-12 col-sm-12`}>
+                    <img src={bgImg} />
                 </div>
                 <div className="blogDescription col-md-12 col-12 col-sm-12">
                     blogDescription
