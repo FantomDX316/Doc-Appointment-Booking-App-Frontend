478d2e1b6b2d23c2ed9a877e8ac6cf278bc5bc41
saksham <phantomdx316@gmail.com>
2024-01-29 22:26:35 +0530

    changes in the blog detail page

--- src/Pages/Blog/BlogDetails.js
+++ src/Pages/Blog/BlogDetails.js
@@ -1,11 +1,27 @@
-import React from 'react'
+// ------------------------------------------------------------Imports------------------------------------------------------
+import React from 'react';
+import styles from "./BlogDetails.module.css";
+// --------------------------------------------------------------------------------------------------------------------------
 
 const BlogDetails = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='container'>
+            <div className='row p-5'>
+                <div className={`${styles.blogTitle} col-md-12 col-12 col-sm-12 text-center`}>
+                    <h1>Blog Title</h1>
+                </div>
+                <div className="blogCreatorDetails col-md-12 col-12 col-sm-12">
+                    blogCreatorDetails
+                </div>
+                <div className="blogImage col-md-12 col-12 col-sm-12">
+                    blogImage
+                </div>
+                <div className="blogDescription col-md-12 col-12 col-sm-12">
+                    blogDescription
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default BlogDetails
