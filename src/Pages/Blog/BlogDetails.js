19f2dd9a4da1f4e0621e6558fe49dc1292bbec79
Saksham <saksham@pearlorganisation.com>
2024-02-01 12:26:18 +0530

    integrated the data in the blog details page

--- src/Pages/Blog/BlogDetails.js
+++ src/Pages/Blog/BlogDetails.js
@@ -1,40 +1,56 @@
 // ------------------------------------------------------------Imports------------------------------------------------------
-import React from 'react';
+import React from "react";
 import styles from "./BlogDetails.module.css";
-import bgImg from "../../Assets/Images/bgImg.jpg"
-import profileImg from "../../Assets/Images/defaultProfileImage.jpg"
+import bgImg from "../../Assets/Images/bgImg.jpg";
+import profileImg from "../../Assets/Images/defaultProfileImage.jpg";
+import { useLocation } from "react-router-dom";
 // --------------------------------------------------------------------------------------------------------------------------
 
 const BlogDetails = () => {
-    return (
-        <div className='container'>
-            <div className='row p-5'>
-                <div className={`${styles.blogTitle} col-md-12 col-12 col-sm-12 text-center`}>
-                    <h1>Blog Title</h1>
-                </div>
-                <div className="blogCreatorDetails col-md-12 col-12 col-sm-12">
-                    <div className={`${styles.blogCreatorDetailsWrapper} d-flex align-items-center justify-content-start col-md-12 col-12 col-sm-12`}>
-                        <div className={`${styles.blogCreatorImageWrapper} col-md-2`}>
-                            <div className={`${styles.blogCreatorImage}`} style={{ backgroundImage: `url(${profileImg})` }}>
+  // ---------------------------------------------------------Hooks---------------------------------------------------------
+  const location = useLocation();
 
-                            </div>
-                        </div>
-                        <div className={`${styles.blogCreatorDetail} d-flex align-items-center justify-content-start col-md-10`}>
-                            <h5>Creator name</h5>
-                        </div>
-                    </div>
-                </div>
-                <div className={`${styles.blogImage} col-md-12 col-12 col-sm-12`}>
-                    <img src={bgImg} />
-                </div>
-                <div className="blogDescription col-md-12 col-12 col-sm-12 mt-3">
-                    <div className={`${styles.blogDescWrapper} col-md-12 col-sm-12 col-12 d-flex justify-content-center p-3`}>
-                        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus magnam recusandae veritatis omnis praesentium quae sint saepe dolorum perspiciatis quis suscipit, fugit tempora iure quam inventore vitae mollitia animi vel nesciunt incidunt! Natus ipsa voluptatem a! Esse blanditiis a eius et ipsam illo possimus totam debitis enim dolorum? Facere?</h4>
-                    </div>
-                </div>
-            </div>
+  const blog = location?.state?.blog || {};
+
+  // --------------------------------------------------------------------------------------------------------------------------
+  return (
+    <div className="container">
+      <div className="row p-5">
+        <div
+          className={`${styles.blogTitle} col-md-12 col-12 col-sm-12 text-center`}
+        >
+          <h1>{blog?.title || "N.A"}</h1>
         </div>
-    )
-}
+        <div className="blogCreatorDetails col-md-12 col-12 col-sm-12">
+          <div
+            className={`${styles.blogCreatorDetailsWrapper} d-flex align-items-center justify-content-start col-md-12 col-12 col-sm-12`}
+          >
+            <div className={`${styles.blogCreatorImageWrapper} col-md-2`}>
+              <div
+                className={`${styles.blogCreatorImage}`}
+                style={{ backgroundImage: `url(${profileImg})` }}
+              ></div>
+            </div>
+            <div
+              className={`${styles.blogCreatorDetail} d-flex align-items-center justify-content-start col-md-10`}
+            >
+              <h5>Creator name</h5>
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.blogImage} col-md-12 col-12 col-sm-12`}>
+          <img src={bgImg} />
+        </div>
+        <div className="blogDescription col-md-12 col-12 col-sm-12 mt-3">
+          <div
+            className={`${styles.blogDescWrapper} col-md-12 col-sm-12 col-12 d-flex justify-content-center p-3`}
+          >
+            <h4>{blog?.description || "N.A"}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BlogDetails
+export default BlogDetails;
