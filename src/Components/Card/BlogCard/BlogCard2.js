2c1dc7bafcb1a833ee71be3bee7e2cc13b8581c1
Saksham <saksham@pearlorganisation.com>
2024-02-01 12:17:29 +0530

    integrated the get blogs api

--- src/Components/Card/BlogCard/BlogCard2.js
+++ src/Components/Card/BlogCard/BlogCard2.js
@@ -1,27 +1,35 @@
 // ------------------------------------------------------------Imports-----------------------------------------------------------------
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-const BlogCard2 = () => {
-
-    // --------------------------------------------------------------Hooks------------------------------------------------------------------
-    const navigate = useNavigate()
-    // -------------------------------------------------------------------------------------------------------------------------------------
-    return (
-        <div>
-            <div class="card" style={{ width: "18rem", margin: "10px", cursor: "pointer" }} onClick={() => { navigate("/blog-details") }}>
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp" class="card-img-top" height="200px" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-            </div>
+const BlogCard2 = ({ blog }) => {
+  // --------------------------------------------------------------Hooks------------------------------------------------------------------
+  const navigate = useNavigate();
+  // -------------------------------------------------------------------------------------------------------------------------------------
+  return (
+    <div>
+      <div
+        class="card"
+        style={{ width: "18rem", margin: "10px", cursor: "pointer" }}
+        onClick={() => {
+          navigate("/blog-details", { state: { blog } });
+        }}
+      >
+        <img
+          src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
+          class="card-img-top"
+          height="200px"
+          alt="..."
+        />
+        <div class="card-body">
+          <h5 class="card-title">{blog?.title || "N.A"}</h5>
+          <p class="card-text">{blog?.description || "N.A"}</p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default BlogCard2
+export default BlogCard2;
--- src/Pages/Blog/Blogs.js
+++ src/Pages/Blog/Blogs.js
@@ -1,31 +1,59 @@
 // -----------------------------------------------------------Imports---------------------------------------------------------
-import React from 'react';
-import BlogCard from '../../Components/Card/BlogCard/BlogCard';
-import BlogCard2 from '../../Components/Card/BlogCard/BlogCard2';
+import React from "react";
+import BlogCard from "../../Components/Card/BlogCard/BlogCard";
+import BlogCard2 from "../../Components/Card/BlogCard/BlogCard2";
 import styles from "./Blogs.module.css";
-// ----------------------------------------------------------------------------------------------------------------------------
+import { useSelector } from "react-redux";
+// -------------------------------------------------------------------------------------------------------------
 
 const Blogs = () => {
+  // ----------------------------------------------------Hooks---------------------------------------------------
+  const blogData = useSelector((state) => state?.blog?.blogData?.Blogs);
+  // -------------------------------------------------------------------------------------------------------------
+  // ----------------------------------------------------Functions------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------
   return (
-    <div className={`${styles.blogsContainer} d-flex justify-content-center flex-wrap`}>
+    <div
+      className={`${styles.blogsContainer} d-flex justify-content-center flex-wrap`}
+    >
       <div className={`${styles.leftContainer} col-md-8 col-sm-8 col-12 `}>
-        <div className={`${styles.blogFilter} col-md-11 col-sm-11 col-11 d-flex align-items-center gap-3`}>
-          {Array(10).fill(0).map((_, index) => { return (<h6>{`Filter ${index + 1}`}</h6>) })}
+        <div
+          className={`${styles.blogFilter} col-md-11 col-sm-11 col-11 d-flex align-items-center gap-3`}
+        >
+          {Array(10)
+            .fill(0)
+            .map((_, index) => {
+              return <h6>{`Filter ${index + 1}`}</h6>;
+            })}
         </div>
-        <div className={`${styles.blogsWrapper}  col-md-11 col-11 col-sm-11 p-5 d-flex flex-column justify-content-center align-items-center`}>
-          {Array(3).fill(0).map(() => <BlogCard />)}
+        <div
+          className={`${styles.blogsWrapper}  col-md-11 col-11 col-sm-11 p-5 d-flex flex-column justify-content-center align-items-center`}
+        >
+          {Array.isArray(blogData) &&
+            blogData.length > 0 &&
+            blogData.map((blog) => {
+              return <BlogCard blog={blog} />;
+            })}
         </div>
       </div>
       <div className={`${styles.rightContainer} col-md-4 col-sm-4 col-12 `}>
-        <div className={`${styles.rightContainerTitle} col-md-12 col-sm-12 col-12 `}>
+        <div
+          className={`${styles.rightContainerTitle} col-md-12 col-sm-12 col-12 `}
+        >
           Top Picks
         </div>
-        <div className={`${styles.rightContainerBlog} col-md-12 col-12 col-sm-12 d-flex flex-column align-items-center `}>
-          {Array(3).fill(0).map(() => <BlogCard2 />)}
+        <div
+          className={`${styles.rightContainerBlog} col-md-12 col-12 col-sm-12 d-flex flex-column align-items-center `}
+        >
+          {Array.isArray(blogData) &&
+            blogData.length > 0 &&
+            blogData.map((blog) => {
+              return <BlogCard2 blog={blog} />;
+            })}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
