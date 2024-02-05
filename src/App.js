a59bbac54aaaa76c7749a19fbdbcdd3d79b2c3ff
Saksham <saksham@pearlorganisation.com>
2024-02-05 15:02:09 +0530

    created the counselor profile

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import {
   RouterProvider,
   useNavigate,
 } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Main from "./Layouts/Main/Main";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Authentication/Login/Login';
-import SignUp from './Pages/Authentication/SignUp/SignUp';
-import Therapy from './Pages/Therapy/Therapy';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Authentication/Login/Login";
+import SignUp from "./Pages/Authentication/SignUp/SignUp";
+import Therapy from "./Pages/Therapy/Therapy";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { clearReduxStoreData } from "./Features/Slices/Authentication/authenticationSlice";
@@ -23,25 +23,27 @@ import CreateBlog from "./Pages/Blog/CreateBlog";
 import MyBlogs from "./Pages/Blog/MyBlogs";
 import { CommonStateProvider } from "./Context/CommonContext/CommonStateProvider";
 import BlogDetails from "./Pages/Blog/BlogDetails";
-import { counselorChecker, isCounselorProfileUpdated } from "./utils/utilsIndex.js";
+import {
+  counselorChecker,
+  isCounselorProfileUpdated,
+} from "./utils/utilsIndex.js";
 import CounselorsList from "./Pages/Counsellor/CounselorsList";
+import CounselorProfile from "./Pages/Counsellor/CounselorProfile.js";
 
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
-
 function App() {
-
-
   // ---------------------------------------------------------------States------------------------------------------------------------------
   // ---------------------------------------------------------------------------------------------------------------------------------------
   // ---------------------------------------------------------------Hooks------------------------------------------------------------------
-  const { isUserLoggedIn, loggedInUserData } = useSelector((state) => state?.authentication)
+  const { isUserLoggedIn, loggedInUserData } = useSelector(
+    (state) => state?.authentication
+  );
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   // ---------------------------------------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------Functions------------------------------------------------------------------
-
 
   // ---------------------------------------------------------------------------------------------------------------------------------------
   // -------------------------------------------------------------useEffect------------------------------------------------------------------
@@ -49,71 +51,169 @@ function App() {
     if (!localStorage.getItem("bearerToken")) {
       dispatch(clearReduxStoreData());
     }
-  }, [])
+  }, []);
 
   // ---------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
   // ---------------------------------------------------------------------------------------------------------------------------------------
 
-  const appRouter = createBrowserRouter([{
-    path: "/",
-    element: <Main />,
-    children: [{
-      path: "/",
-      element: isUserLoggedIn ? <Home /> : <Navigate to="/login" />
-    }, {
-      path: "/login",
-      element: !isUserLoggedIn ? <Login /> : <Navigate to="/" />
-    }, {
-      path: "/signup",
-      element: !isUserLoggedIn ? <SignUp /> : <Navigate to="/" />
-    }, {
-      path: "/therapy",
-      element: isUserLoggedIn ? !counselorChecker(loggedInUserData) ? <Therapy /> : <Navigate to="/" /> : <Navigate to="/login" />
-    }, {
-      path: "/counselor",
-      element: isUserLoggedIn ? counselorChecker(loggedInUserData) ? isCounselorProfileUpdated(loggedInUserData) ? <Profile /> : <Counsellor /> : <Navigate to="/" /> : <Navigate to="/login" />
-    },
+  const appRouter = createBrowserRouter([
     {
-      path: "/counselors-list",
-      element: isUserLoggedIn ? !counselorChecker(loggedInUserData) ? <CounselorsList /> : <Navigate to="/" /> : <Navigate to="/login" />
-    }
-      , {
-      path: "/profile",
-      element: isUserLoggedIn ? counselorChecker(loggedInUserData) ? isCounselorProfileUpdated(loggedInUserData) ? <Profile /> : <Counsellor /> : <Profile /> : <Navigate to="/login" />
-    }
-      , {
-      path: "/blogs",
-      element: isUserLoggedIn ? counselorChecker(loggedInUserData) ? isCounselorProfileUpdated(loggedInUserData) ? <Blogs /> : <Counsellor /> : <Blogs /> : <Navigate to="/login" />
-    }
-      , {
-      path: "/create-blog",
-      element: isUserLoggedIn ? counselorChecker(loggedInUserData) ? isCounselorProfileUpdated(loggedInUserData) ? <CreateBlog /> : <Counsellor /> : <Navigate to="/" /> : <Navigate to="/login" />
-    }
-      , {
-      path: "/my-blogs",
-      element: isUserLoggedIn ? counselorChecker(loggedInUserData) ? isCounselorProfileUpdated(loggedInUserData) ? <MyBlogs /> : <Counsellor /> : <Navigate to="/" /> : <Navigate to="/login" />
-    }
-      , {
-      path: "/blog-details",
-      element: isUserLoggedIn ? counselorChecker(loggedInUserData) ? isCounselorProfileUpdated(loggedInUserData) ? <BlogDetails /> : <Counsellor /> : <BlogDetails /> : <Navigate to="/login" />
-    }
-
-    ]
-  }]);
+      path: "/",
+      element: <Main />,
+      children: [
+        {
+          path: "/",
+          element: isUserLoggedIn ? <Home /> : <Navigate to="/login" />,
+        },
+        {
+          path: "/login",
+          element: !isUserLoggedIn ? <Login /> : <Navigate to="/" />,
+        },
+        {
+          path: "/signup",
+          element: !isUserLoggedIn ? <SignUp /> : <Navigate to="/" />,
+        },
+        {
+          path: "/therapy",
+          element: isUserLoggedIn ? (
+            !counselorChecker(loggedInUserData) ? (
+              <Therapy />
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/counselor",
+          element: isUserLoggedIn ? (
+            counselorChecker(loggedInUserData) ? (
+              isCounselorProfileUpdated(loggedInUserData) ? (
+                <Profile />
+              ) : (
+                <Counsellor />
+              )
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/counselors-list",
+          element: isUserLoggedIn ? (
+            !counselorChecker(loggedInUserData) ? (
+              <CounselorsList />
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/counselor-profile",
+          element: isUserLoggedIn ? (
+            !counselorChecker(loggedInUserData) ? (
+              <CounselorProfile />
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/profile",
+          element: isUserLoggedIn ? (
+            counselorChecker(loggedInUserData) ? (
+              isCounselorProfileUpdated(loggedInUserData) ? (
+                <Profile />
+              ) : (
+                <Counsellor />
+              )
+            ) : (
+              <Profile />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/blogs",
+          element: isUserLoggedIn ? (
+            counselorChecker(loggedInUserData) ? (
+              isCounselorProfileUpdated(loggedInUserData) ? (
+                <Blogs />
+              ) : (
+                <Counsellor />
+              )
+            ) : (
+              <Blogs />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/create-blog",
+          element: isUserLoggedIn ? (
+            counselorChecker(loggedInUserData) ? (
+              isCounselorProfileUpdated(loggedInUserData) ? (
+                <CreateBlog />
+              ) : (
+                <Counsellor />
+              )
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/my-blogs",
+          element: isUserLoggedIn ? (
+            counselorChecker(loggedInUserData) ? (
+              isCounselorProfileUpdated(loggedInUserData) ? (
+                <MyBlogs />
+              ) : (
+                <Counsellor />
+              )
+            ) : (
+              <Navigate to="/" />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+        {
+          path: "/blog-details",
+          element: isUserLoggedIn ? (
+            counselorChecker(loggedInUserData) ? (
+              isCounselorProfileUpdated(loggedInUserData) ? (
+                <BlogDetails />
+              ) : (
+                <Counsellor />
+              )
+            ) : (
+              <BlogDetails />
+            )
+          ) : (
+            <Navigate to="/login" />
+          ),
+        },
+      ],
+    },
+  ]);
 
   // ---------------------------------------------------------------------------------------------------------------------------------------
-
 
   // -----------------------------------------------------------------------------------------------------------------------------------------
   return (
-
     <>
       <CommonStateProvider>
         <RouterProvider router={appRouter}>
@@ -122,9 +222,7 @@ function App() {
       </CommonStateProvider>
       <ToastContainer autoClose={1000} position="top-center" />
     </>
-
-
-  )
+  );
 }
 
 export default App;
--- src/Components/Card/CounselorCard/CounselorCard.js
+++ src/Components/Card/CounselorCard/CounselorCard.js
@@ -1,9 +1,15 @@
+// ---------------------------------------------Imports--------------------------------------------
 import React from "react";
 import styles from "./CounselorCard.module.css";
+import { useNavigate } from "react-router";
+// -------------------------------------------------------------------------------------------------
 
 const CounselorCard = () => {
+  // --------------------------------------------Hooks----------------------------------------------
+  const navigate = useNavigate();
+  // -------------------------------------------------------------------------------------------------
   return (
-    <div className={`${styles.card}`}>
+    <div className={`${styles.card}`} onClick={()=>{navigate("/counselor-profile")}}>
       <img
         src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D"
         alt=""
@@ -30,7 +36,10 @@ const CounselorCard = () => {
             </>
           </div>
         </p>
-        <div className={`${styles.bookBtn} text-center`} style={{ color: "blue" }}>
+        <div
+          className={`${styles.bookBtn} text-center`}
+          style={{ color: "blue" }}
+        >
           Book Now -
         </div>
       </div>
