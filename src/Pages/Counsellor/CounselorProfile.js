01ab5344ecd07158386ba248770837452c0b3acb
saksham <phantomdx316@gmail.com>
2024-02-09 21:33:27 +0530

    added appointment icons

--- src/Pages/Counsellor/CounselorProfile.js
+++ src/Pages/Counsellor/CounselorProfile.js
@@ -1,6 +1,8 @@
 // --------------------------------------------------Imports-------------------------------------------
 import React from "react";
 import styles from "./CounselorProfile.module.css";
+import { FaHome,FaVideo } from "react-icons/fa";
+import { IoCallSharp } from "react-icons/io5";
 // -----------------------------------------------------------------------------------------------------
 
 const CounselorProfile = () => {
@@ -26,31 +28,6 @@ const CounselorProfile = () => {
               </div>
             </div>
           </div>
-          <div class="bg-light p-4 d-flex justify-content-end text-center">
-            <ul class="list-inline mb-0">
-              <li class="list-inline-item">
-                <h5 class="font-weight-bold mb-0 d-block">215</h5>
-                <small class="text-muted">
-                  {" "}
-                  <i class="fas fa-image mr-1"></i>Photos
-                </small>
-              </li>
-              <li class="list-inline-item">
-                <h5 class="font-weight-bold mb-0 d-block">745</h5>
-                <small class="text-muted">
-                  {" "}
-                  <i class="fas fa-user mr-1"></i>Followers
-                </small>
-              </li>
-              <li class="list-inline-item">
-                <h5 class="font-weight-bold mb-0 d-block">340</h5>
-                <small class="text-muted">
-                  {" "}
-                  <i class="fas fa-user mr-1"></i>Following
-                </small>
-              </li>
-            </ul>
-          </div>
           <div class="px-4 py-3">
             <h5 class="mb-0">About</h5>
             <div class="p-4 rounded shadow-sm bg-light">
@@ -59,41 +36,55 @@ const CounselorProfile = () => {
               <p class="font-italic mb-0">Photographer</p>
             </div>
           </div>
-          <div class="py-4 px-4">
+          <div class="px-4 py-3">
+            <h5 class="mb-0">Bio</h5>
+            <div class="p-4 rounded shadow-sm bg-light">
+              <p class="font-italic mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea rerum id veniam harum expedita quis suscipit non. Velit dolores libero minima? Eligendi amet ipsum aliquid illum, architecto ab facere molestias. Nesciunt dolorum voluptatum consectetur minus dolor, natus rerum ratione quia in aspernatur atque libero tempore incidunt. Quidem ipsam id minus?</p>
+            </div>
+          </div>
+          <div class={`${styles.appointmentContainer} py-4 px-4`}>
             <div class="d-flex align-items-center justify-content-between mb-3">
-              <h5 class="mb-0">Recent photos</h5>
-              <a href="#" class="btn btn-link text-muted">
-                Show all
-              </a>
+              <h5 class="mb-0 text-center col-md-12">Book an Appointment</h5>
             </div>
             <div class="row">
-              <div class="col-lg-6 mb-2 pr-lg-1">
-                <img
-                  src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                  alt=""
-                  class="img-fluid rounded shadow-sm"
-                />
-              </div>
-              <div class="col-lg-6 mb-2 pl-lg-1">
-                <img
-                  src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                  alt=""
-                  class="img-fluid rounded shadow-sm"
-                />
-              </div>
-              <div class="col-lg-6 pr-lg-1 mb-2">
-                <img
-                  src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                  alt=""
-                  class="img-fluid rounded shadow-sm"
-                />
-              </div>
-              <div class="col-lg-6 pl-lg-1">
-                <img
-                  src="https://images.unsplash.com/photo-1475724017904-b712052c192a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                  alt=""
-                  class="img-fluid rounded shadow-sm"
-                />
+              <div className="col-md-12 col-sm-12 col-12">
+                <div className="callOptions col-md-12 col-12 col-sm-12 d-flex gap-5 justify-content-center">
+                  <div className={`${styles.callCard} col-md-2 col-sm-2 col-2 d-flex flex-column align-items-center`}>
+                    <div className="callCardLogo col-md-12 col-sm-12 col-12 d-flex align-items-center justify-content-center">
+                      <FaHome size={"50%"} />
+                    </div>
+                    <div className="callCardTitle col-md-12 col-sm-12 col-12 p-3 d-flex justify-content-center align-items-center">
+                      <h5>In Person</h5>
+                    </div>
+                  </div>
+                  <div className={`${styles.callCard} col-md-2 col-sm-2 col-2 d-flex flex-column align-items-center`}>
+                    <div className="callCardLogo col-md-12 col-sm-12 col-12 d-flex align-items-center justify-content-center">
+                      <FaVideo size={"50%"} />
+                    </div>
+                    <div className="callCardTitle col-md-12 col-sm-12 col-12 p-3 d-flex justify-content-center align-items-center">
+                      <h5>Video</h5>
+                    </div>
+                  </div>
+                  <div className={`${styles.callCard} col-md-2 col-sm-2 col-2 d-flex flex-column align-items-center`}>
+                    <div className="callCardLogo col-md-12 col-sm-12 col-12 d-flex align-items-center justify-content-center">
+                      <IoCallSharp size={"50%"} />
+                    </div>
+                    <div className="callCardTitle col-md-12 col-sm-12 col-12 p-3 d-flex justify-content-center align-items-center">
+                      <h5>Call</h5>
+                    </div>
+                  </div>
+                </div>
+                <div className="counselorLocation col-md-12 col-12 col-sm-12 text-center mt-4">
+                  <h4>Demo Health Center</h4>
+                  <h5>601, Notan Heights, 18A, Gurunanak Rd, Mumbai</h5>
+                  <h6>Mumbai centre +912071171501</h6>
+                </div>
+                <div className="locationCarousel">
+                  
+                </div>
+                <div className="sessionDuration">
+
+                </div>
               </div>
             </div>
           </div>
