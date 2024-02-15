35c514f638226e85ca05fdd0b724a7a0c34913a9
Saksham <saksham@pearlorganisation.com>
2024-02-15 15:12:34 +0530

    integrated the upload video api

--- src/Components/Modal/Video/VideoModal.jsx
+++ src/Components/Modal/Video/VideoModal.jsx
@@ -1,8 +1,64 @@
-//----------------------------------------------Imports
-import React from "react";
+//----------------------------------------------Imports---------------------------------------------
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { counselorIntroductoryVideos } from "../../../Features/Actions/Counselor/counselorActions";
+import { useNavigate } from "react-router-dom";
+import { resetCounselorState } from "../../../Features/Slices/Counselor/counselorSlice";
 // -------------------------------------------------------------------------------------------------
 
 const VideoModal = ({}) => {
+  //----------------------------------------------States---------------------------------------------
+  const [videoData, setVideoData] = useState([]);
+  //-------------------------------------------------------------------------------------------------
+  //----------------------------------------------Hooks---------------------------------------------
+  const dispatch = useDispatch();
+  const { isCounselorLoading, isCounselorUpdated } = useSelector(
+    (state) => state.counselor
+  );
+  const { isUserLoggedIn, loggedInUserData } = useSelector(
+    (state) => state?.authentication
+  );
+  const closeRef = useRef();
+
+  const navigate = useNavigate();
+  // -------------------------------------------------------------------------------------------------
+  // --------------------------------------------Functions---------------------------------------------
+  // videoHandler -- handler to handle the change in the videos
+  const videoHandler = (e) => {
+    setVideoData(e.target.files);
+  };
+
+  // videoUploadHandler -- handler to upload the videos
+  const videoUploadHandler = () => {
+    const formData = new FormData();
+    if (videoData.length === 0) {
+      toast.error("Please Select Videos");
+    } else {
+      if (!loggedInUserData?.decodedData?.counselorId) {
+        toast.error("Please Provide the Counselor ID");
+        navigate("/");
+      } else {
+        formData.append("video", videoData);
+        dispatch(
+          counselorIntroductoryVideos({
+            payload: formData,
+            counselorId: loggedInUserData?.decodedData?.counselorId,
+          })
+        );
+      }
+    }
+  };
+  // -------------------------------------------------------------------------------------------------
+  // ---------------------------------------------useEffects------------------------------------------
+  useEffect(() => {
+    if (isCounselorUpdated) {
+      resetCounselorState(false);
+      closeRef.current.click();
+    }
+  }, [isCounselorUpdated]);
+  // -------------------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------
   return (
     <div>
       <button
@@ -38,17 +94,31 @@ const VideoModal = ({}) => {
               ></button>
             </div>
             <div className="modal-body">
-              <input type="file" accept="video/*" multiple />
+              {isCounselorLoading ? (
+                <h1>Loading...</h1>
+              ) : (
+                <input
+                  type="file"
+                  accept="video/*"
+                  multiple
+                  onChange={videoHandler}
+                />
+              )}
             </div>
             <div className="modal-footer">
               <button
                 type="button"
                 className="btn btn-secondary"
                 data-bs-dismiss="modal"
+                ref={closeRef}
               >
                 Close
               </button>
-              <button type="button" className="btn btn-primary">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={videoUploadHandler}
+              >
                 Upload Videos
               </button>
             </div>
